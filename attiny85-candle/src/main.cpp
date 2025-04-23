f28c57c4eb47164915ac033f677ca0eb2d5b17f8
Tony Fischetti <tony.fischetti@gmail.com>
2025-04-22 20:53:32 -0400

    candle improvements

--- attiny85-candle/src/main.cpp
+++ attiny85-candle/src/main.cpp
@@ -1,25 +1,29 @@
+#include <stdint.h>
+#include <stdlib.h>
+
 #include <avr/io.h>
 #include <avr/power.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <avr/sleep.h>
 
+#include <util/atomic.h>
+
 #include "attiny85-hal.hpp"
-#include "utils.h"
+
 
 using LED1 = GPIO<5>;
 using LED2 = GPIO<6>;
 using LED3 = GPIO<7>;
 
+volatile bool    flicker_p    { false };
+volatile uint8_t current_rand { 0 };
 
-volatile bool    flickerP    { false };
-volatile uint8_t currentRand { 0 };
-
-volatile uint32_t tickCounter { 0 }; // 125ms currently  TODO 
+volatile uint32_t tick_counter { 0 };
 
 
 ISR(WDT_vect) {
-    flickerP = true;
+    flicker_p = true;
     // tickCounter++;
 }
 
@@ -29,43 +33,15 @@ ISR(WDT_vect) {
 // }
 
 
-/*
-uint32_t getTickCounter() {
+uint32_t get_tick_counter() {
   uint32_t value;
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
-    value = tickCounter;
+    value = tick_counter;
   }
   return value;
 }
-*/
 
-
-void flicker() {
-    flickerP = false;
-
-    PORTB = 0;
-
-    currentRand = static_cast<uint8_t>(rand());
-
-    if ((currentRand & (0x03) << 0)) LED1::setHigh();
-    if ((currentRand & (0x03) << 2)) LED2::setHigh();
-    if ((currentRand & (0x03) << 4) ||
-         !currentRand) LED3::setHigh();
-
-    // make watchdog use interrupts (not resets) again
-    WDTCR |= (1 << WDCE);
-
-}
-
-
-int main() {
-
-    // power_adc_disable();
-    // power_timer0_disable();
-    // power_timer1_disable();
-    // power_usi_disable();
-    power_all_disable();
-
+void reset_watchdog() {
 	cli();
 
     // MCUSR - MCU Status Register
@@ -84,11 +60,46 @@ int main() {
     // WDP1 = 1
     // WDP0 = 0
 
-    // WDTCR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1); // 1 second
     WDTCR = (1 << WDIE) | (1 << WDP1) | (1 << WDP0); // 125 ms
+    // WDTCR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1); // 1 second
     // WDTCR = (1 << WDIE) | (1 << WDP1);    // 64 ms
     // WDTCR = (1 << WDIE) | (1 << WDP2) | (1 << WDP1) | (1 << WDP0); // 2 seconds
 
+	// Enable all interrupts.
+	sei();
+}
+
+void go_to_sleep() {
+    cli();
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+    sei();
+    sleep_cpu();
+    sleep_disable();
+}
+
+void flicker() {
+    flicker_p = false;
+
+    current_rand = static_cast<uint8_t>(rand());
+
+    if ((current_rand & (0x03) << 0)) LED1::setHigh();
+    else                              LED1::setLow();
+    if ((current_rand & (0x03) << 2)) LED2::setHigh();
+    else                              LED2::setLow();
+    if ((current_rand & (0x03) << 4) ||
+         !current_rand) LED3::setHigh();
+    else LED3::setLow();
+}
+
+
+int main() {
+
+    // power_adc_disable();
+    // power_timer0_disable();
+    // power_timer1_disable();
+    // power_usi_disable();
+    power_all_disable();
 
     /*
     // Pin Change Interrupt on PB1 (pin 6)
@@ -97,19 +108,14 @@ int main() {
     PCMSK |= (1 << PCINT1);          // Enable PB1 (PCINT1)
     */
 
-
-	// Enable all interrupts.
-	sei();
-
     LED1::setOutput();
     LED2::setOutput();
     LED3::setOutput();
-
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-    sleep_enable();
+    
+    reset_watchdog();
 
     while (1) {
-        if (flickerP) flicker();
+        if (flicker_p) flicker();
 
         /*
         if (buttonPressed) {
@@ -118,9 +124,8 @@ int main() {
         }
         */
 
-        sleep_enable();
-        sleep_cpu();
-        sleep_disable();
+        reset_watchdog();
+        go_to_sleep();
     }
 
     return 0;
