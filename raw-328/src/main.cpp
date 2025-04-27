52a62c516bfb2437522b314efaa3c8df879794bc
Tony Fischetti <tony.fischetti@gmail.com>
2025-04-27 16:04:21 -0400

    tested makefile flashing to raw atmega328p

---
+++ raw-328/src/main.cpp
@@ -0,0 +1,16 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void) {
+
+    DDRB = 0b11111111;
+
+    while (1) {
+        PORTB = 0b11111111;
+        _delay_ms(500);
+        PORTB = 0b00000000;
+        _delay_ms(500);
+    }
+    return 0;
+}
