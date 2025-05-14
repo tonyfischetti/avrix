1265a231b64b10af73522efef523d3ca7111c3ad
Tony Fischetti <tony.fischetti@gmail.com>
2025-05-14 14:16:12 -0500

    lfsr is now maximally long, and checked carefully

--- asm/snippets/lfsr.asm
+++ asm/snippets/lfsr.asm
@@ -1,16 +1,19 @@
-
 .section .data
 
 .section .text
-.global get_next_bit
+.global get_next_byte
 
-ldi r16, 93 ; starting seed / state of lfsr
+; uses r16 hold state
+; r17 holds the next random byte
+; r18 is clobbered
+; r19 is clobbered
+
+
+init_lfsr:
+    ldi r16, 93 ; starting seed / state of lfsr
 
 get_next_byte:
-    ; uses r16 hold state
     clr r17     ; final rand byte
-    ; clobbers r18 (used for construction of fresh state)
-    ; clobbers r19 (used for construction of fresh bit [XOR]s)
     call get_next_bit
     call get_next_bit
     call get_next_bit
@@ -21,22 +24,20 @@ get_next_byte:
     call get_next_bit
     ret
 
-
 get_next_bit:
     clr r19     ; for construction of next bit
     mov r18, r16
-    
+    eor r19, r16 ; r16 >> 0
     lsr r16      ; r16 >> 1   
     lsr r16      ; r16 >> 2
+    eor r19, r16 ;
     lsr r16      ; r16 >> 3
-    mov r19, r16 ; 
+    eor r19, r16 ;
     lsr r16      ; r16 >> 4
     eor r19, r16 ;
     lsr r16      ; r16 >> 5
-    eor r19, r16 ;
     lsr r16      ; r16 >> 6
     lsr r16      ; r16 >> 7
-    eor r19, r16 ;
     lsr r19      ; carry flag is the fresh bit!
     ror r18      ; carry flag is the random bit and r18 is new state
     rol r17    
